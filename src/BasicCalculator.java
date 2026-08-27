7d89c94bd963caf148979e7b1d03466dee98bd39
Antar <eha087@gmail.com>
2026-08-28 00:50:31 +0600

    BasicCalculator: difference between integer division vs double division, typecasting

---
+++ src/BasicCalculator.java
@@ -0,0 +1,9 @@
+public class BasicCalculator {
+    public static void main(String[] args){
+        int a=5,b=2;
+        double c=5,d=2;
+        System.out.println(a/b);
+        System.out.println(c/d);
+        System.out.println((double)a/b);
+    }
+}
